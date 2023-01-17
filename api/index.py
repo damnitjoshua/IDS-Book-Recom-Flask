e58ace726bb061110363b7183baef99cada7425e
Joshua Chew <joshuachew8118@gmail.com>
2023-01-17 23:15:45 +0800

    wip

--- api/index.py
+++ api/index.py
@@ -5,8 +5,6 @@ import pandas as pd
 
 app = Flask(__name__)
 
-model = pickle.load(open('model/knn_bookRecom_model.sav', 'rb'))
-
 # Rating count more than 50
 csv_url = "https://drive.google.com/file/d/1tIxt00bOAPEKRkc57uuBhPZGDLBwsvJc/view?usp=share_link"
 csv_url = 'https://drive.google.com/uc?id=' + csv_url.split('/')[-2]
@@ -41,23 +39,24 @@ def random(count):
     return books
 
 
-# @app.route('/knn/<ISBN>')
-# def knn(ISBN):
-#     search = RatingCountDFPivot.loc[ISBN]
+@app.route('/knn/<ISBN>')
+def knn(ISBN):
+    model = pickle.load(open('model/knn_bookRecom_model.sav', 'rb'))
+    search = RatingCountDFPivot.loc[ISBN]
 
-#     distances, indices = model.kneighbors(
-#         search.values.reshape(1, -1), n_neighbors=6)
+    distances, indices = model.kneighbors(
+        search.values.reshape(1, -1), n_neighbors=6)
 
-#     books = []
+    books = []
 
-#     for i in range(0, len(distances.flatten())):
-#         if i != 0:
-#             book = RatingCountDF.iloc[indices.flatten()[i]]
-#             books.append({
-#                 "title": book['Title'],
-#                 "isbn": book['ISBN'],
-#                 "image": book['Image'],
-#                 "distance": distances.flatten()[i]
-#             })
+    for i in range(0, len(distances.flatten())):
+        if i != 0:
+            book = RatingCountDF.iloc[indices.flatten()[i]]
+            books.append({
+                "title": book['Title'],
+                "isbn": book['ISBN'],
+                "image": book['Image'],
+                "distance": distances.flatten()[i]
+            })
 
-#     return books[::-1]
+    return books[::-1]
