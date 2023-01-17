22fa70aecd100357f5294b1f68914ee71a27a09a
Joshua Chew <joshuachew8118@gmail.com>
2023-01-17 20:36:29 +0800

    try default

--- api/index.py
+++ api/index.py
@@ -5,14 +5,14 @@ import pandas as pd
 
 app = Flask(__name__)
 
-model = pickle.load(open('model/knn_bookRecom_model.sav', 'rb'))
+# model = pickle.load(open('model/knn_bookRecom_model.sav', 'rb'))
 
 # Rating count more than 50
-csv_url = "https://drive.google.com/file/d/1tIxt00bOAPEKRkc57uuBhPZGDLBwsvJc/view?usp=share_link"
-csv_url = 'https://drive.google.com/uc?id=' + csv_url.split('/')[-2]
-RatingCountDF = pd.read_csv(csv_url)
-RatingCountDFPivot = RatingCountDF.pivot(
-    index='ISBN', columns='UserID', values='Rating').fillna(0)
+# csv_url = "https://drive.google.com/file/d/1tIxt00bOAPEKRkc57uuBhPZGDLBwsvJc/view?usp=share_link"
+# csv_url = 'https://drive.google.com/uc?id=' + csv_url.split('/')[-2]
+# RatingCountDF = pd.read_csv(csv_url)
+# RatingCountDFPivot = RatingCountDF.pivot(
+#     index='ISBN', columns='UserID', values='Rating').fillna(0)
 
 
 @app.route('/')
@@ -20,44 +20,44 @@ def home():
     return 'Hello, World!'
 
 
-@app.route('/random/<count>')
-def random(count):
-    books = []
+# @app.route('/random/<count>')
+# def random(count):
+#     books = []
 
-    for i in range(int(count)):
-        query_index = np.random.choice(RatingCountDF.shape[0])
-        book = RatingCountDF.iloc[query_index]
-        books.append({
-            "title": book['Title'],
-            "author": book['Author'],
-            "isbn": book['ISBN'],
-            "image": book['Image'],
-            "category": book["Category"][2:-2],
-            "rating": str(book['Rating']),
-            "ratingCount": str(book["RatingCount"]),
-            "yearOfPublication": str(book["YearOfPublication"])
-        })
+#     for i in range(int(count)):
+#         query_index = np.random.choice(RatingCountDF.shape[0])
+#         book = RatingCountDF.iloc[query_index]
+#         books.append({
+#             "title": book['Title'],
+#             "author": book['Author'],
+#             "isbn": book['ISBN'],
+#             "image": book['Image'],
+#             "category": book["Category"][2:-2],
+#             "rating": str(book['Rating']),
+#             "ratingCount": str(book["RatingCount"]),
+#             "yearOfPublication": str(book["YearOfPublication"])
+#         })
 
-    return books
+#     return books
 
 
-@app.route('/knn/<ISBN>')
-def knn(ISBN):
-    search = RatingCountDFPivot.loc[ISBN]
+# @app.route('/knn/<ISBN>')
+# def knn(ISBN):
+#     search = RatingCountDFPivot.loc[ISBN]
 
-    distances, indices = model.kneighbors(
-        search.values.reshape(1, -1), n_neighbors=6)
+#     distances, indices = model.kneighbors(
+#         search.values.reshape(1, -1), n_neighbors=6)
 
-    books = []
+#     books = []
 
-    for i in range(0, len(distances.flatten())):
-        if i != 0:
-            book = RatingCountDF.iloc[indices.flatten()[i]]
-            books.append({
-                "title": book['Title'],
-                "isbn": book['ISBN'],
-                "image": book['Image'],
-                "distance": distances.flatten()[i]
-            })
+#     for i in range(0, len(distances.flatten())):
+#         if i != 0:
+#             book = RatingCountDF.iloc[indices.flatten()[i]]
+#             books.append({
+#                 "title": book['Title'],
+#                 "isbn": book['ISBN'],
+#                 "image": book['Image'],
+#                 "distance": distances.flatten()[i]
+#             })
 
-    return books[::-1]
+#     return books[::-1]
